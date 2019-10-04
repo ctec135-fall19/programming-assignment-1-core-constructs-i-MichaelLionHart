79053b45a1f038f2e5730fdc88a62952881a8764
Michael Hart <M.HART@students.clark.edu>
2019-10-04 15:52:41 -0700

    Changes to Problem 1

--- CS_Core_Constructs_I/Prob1/Program.cs
+++ CS_Core_Constructs_I/Prob1/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
-Author: <your name>
-Date:   <date>
+Author: Mike Hart
+Date:   10/4/19
 CTEC 135: Microsoft Software Development with C#
 
 <Solution Name>
@@ -36,11 +36,45 @@ namespace Prob1
         static void Main(string[] args)
         {
             #region example of a code region
-            // some code
+
+            double i = 0;
+            System.Double j = 0;
+            double m = new double();
+
             #endregion
 
             #region another code region
-            // some code
+
+            bool b = true;
+            byte a = 200;
+            short s = 3000;
+            int x = 200_000_000;
+            long l = 566_754_432_456_345L;
+            float f = 345.654F;
+            double d = 344_987.35;
+            decimal y = 320_874.74M;
+
+            Console.WriteLine("bool b: ", b);
+            Console.WriteLine("byte a in E format: {0:E}", a);
+            Console.WriteLine("short s: ", s);
+            Console.WriteLine("int x: ", x);
+            Console.WriteLine("long l as currency: {0:c}", l);
+            Console.WriteLine("float f: ", f);
+            Console.WriteLine("double d: ", d);
+            Console.WriteLine("decimal y: ", y);
+
+            #endregion
+
+            Console.WriteLine("The range of a byte is: {0} - {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("The range of a short is: {0} - {1}", short.MinValue, short.MaxValue);
+            Console.WriteLine("The range of a an int is: {0} - {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("The range of a long is: {0} - {1}", long.MinValue, long.MaxValue);
+            Console.WriteLine("The range of a float is: {0} - {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("The range of a double is: {0} - {1}", double.MinValue, double.MaxValue);
+            Console.WriteLine("The range of a decimal is: {0} - {1}", decimal.MinValue, decimal.MaxValue);      
+            
+            #region
+
             #endregion
         }
     }
